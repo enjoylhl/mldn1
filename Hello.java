9e9168198d1b590fe8642bd2d4643e75a0a87320
Luhailong <18624090468@163.com>
2020-06-04 09:13:52 +0800

    Add Demo.java File

--- Hello.java
+++ Hello.java
@@ -30,6 +30,7 @@ public class Hello implements IDeptService {
 	}
 	
 	public static void main(String[] args){
-		System.out.println("Hello Yootk");
+		System.out.println("666");
+		System.out.println("www");
 	}
 }
